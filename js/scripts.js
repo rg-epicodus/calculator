a54ef71104afb17c1eac18162ab8ef26970a073f
brian beal and rich garrick <rich@>
2017-05-17 20:58:31 -0700

    add button click functionality

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,26 @@ alert("You did it wrong");
 } else {
 alert("You did it wrong");
 };
+
+
+// function calculator() {
+// var a = parseInt(prompt("Enter the first number"));
+// var b = parseInt(prompt("Enter the second number"));
+// var x = prompt("What type of equation would you like to compute? (+, -, *, /)");
+//
+// if ((isNaN(a) === false) && (isNaN(b) === false)) {
+// if (x === "+") {
+// alert(a+b);
+// } else if (x === "-") {
+// alert(a-b);
+// } else if (x === "*") {
+// alert(a*b);
+// } else if (x === "/") {
+// alert(a/b);
+// } else {
+// alert("You did it wrong");
+// };
+// } else {
+// alert("You did it wrong");
+// };
+// };
